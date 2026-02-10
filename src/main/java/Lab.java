d9f2cbc5f62d94eed1827078f037aa49646e0167
mohammed5000 <mohammed5000@github.com>
2026-02-10 16:23:36 +0000

    Test run: 7/9 passed, 2 failed, 0 skipped - 2026-02-10T16:23:35.998Z

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -45,8 +45,8 @@ public class Lab {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
-        for (int i = val; i < n; ++val) {
-            System.out.print(arr[val] + ", " );
+        for (int i = val; i < n; ++i) {
+            System.out.print(arr[i] + ", " );
         }
     }
 
